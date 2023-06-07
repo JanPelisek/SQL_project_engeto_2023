e959a52e396f057152141ae70b343426b05f9091
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-06-07 17:08:21 +0200

    Update secondary_table.sql

--- secondary_table.sql
+++ secondary_table.sql
@@ -1,3 +1,6 @@
+-- tvorba sekundární tabulky t_jan_pelisek_project_sql_secondary_final
+-- tuto tabulku jsem vytvořil spojením tabulek economies a countries přes sloupec country, data ve výsledné tabulce jsem omezil na sledované období a pouze na evropské země
+
 CREATE TABLE t_jan_pelisek_project_sql_secondary_final AS
 SELECT
 	c.country,
