d6b63e8a97f0ba69d89c6562f9e6e9faa6f0a2b6
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-06-07 17:00:13 +0200

    Update primary_table.sql

--- primary_table.sql
+++ primary_table.sql
@@ -11,7 +11,7 @@ SELECT
 	cpc.price_unit 
 FROM czechia_price cp
 JOIN czechia_price_category cpc
-ON cp.category_code = cpc.code
+	ON cp.category_code = cpc.code
 GROUP BY YEAR(cp.date_from), name 
 
 SELECT * FROM czechia_price_temporary
@@ -23,11 +23,11 @@ SELECT
 	round(avg(CP.value)) AS 'avg_payroll_value' 
 FROM czechia_payroll cp
 LEFT JOIN czechia_payroll_industry_branch cpib
-ON cp.industry_branch_code = cpib.code
+	ON cp.industry_branch_code = cpib.code
 WHERE cp.value_type_code = 5958
-AND cp.calculation_code = 200
-AND cp.payroll_year BETWEEN 2006 AND 2018
-AND cp.industry_branch_code IS NOT NULL
+	AND cp.calculation_code = 200
+	AND cp.payroll_year BETWEEN 2006 AND 2018
+	AND cp.industry_branch_code IS NOT NULL
 GROUP BY cpib.name, cp.payroll_year
 ORDER BY cpib.name, cp.payroll_year
 
@@ -41,6 +41,6 @@ SELECT
 	cpricet.price_unit
 FROM czechia_payroll_temporary cpayt
 JOIN czechia_price_temporary cpricet
-ON cpayt.year = cpricet.year
+	ON cpayt.year = cpricet.year
 
 SELECT * FROM t_jan_pelisek_project_sql_primary_final
