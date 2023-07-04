56d2f6fe4b7aa6d319f42791a799d2eae1e3782d
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-04 14:19:11 +0200

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -1,3 +1,5 @@
+-- při tvorbě tohoto dotazu jsem využil selecty z předchozího dotazu
+-- začal jsem základním pohledem na data cen produktů
 CREATE OR REPLACE VIEW v_q4price_1 AS
 SELECT DISTINCT 
 	`year`,
@@ -6,17 +8,20 @@ SELECT DISTINCT
 FROM t_jan_pelisek_project_sql_primary_final
 ORDER BY product_name, `year`
 
+-- přidal jsem LAG() funkci pro zobrazení předchozích hodnot cen
 CREATE OR REPLACE VIEW v_q4price_2 AS
 SELECT 
 	*,
 	LAG(avg_price_value, 1) OVER (PARTITION BY product_name ORDER BY `year` ASC) AS previous_price_value
 FROM v_q4price_1
 
+-- přidal jsem výpočet meziročního procentuálního nárustu
 CREATE OR REPLACE VIEW v_q4price_3 AS
 SELECT *,
 	round(((avg_price_value - previous_price_value)/ previous_price_value) * 100, 2) AS perc_narust_price
 FROM v_q4price_2
 
+-- do finální tabulky s daty o cenách jsem dal výpočet průměrného procentuálního nárůstu
 CREATE OR REPLACE TEMPORARY TABLE tt_pricerise
 SELECT 
 	`year`,
@@ -24,6 +29,7 @@ SELECT
 FROM v_q4price_3
 GROUP BY `year`
 
+-- základní pohled na data mezd
 CREATE OR REPLACE VIEW v_q4pay_1 AS
 SELECT DISTINCT
 	`year`,
@@ -32,17 +38,20 @@ SELECT DISTINCT
 FROM t_jan_pelisek_project_sql_primary_final
 ORDER BY product_name, `year`
 
+-- přidal jsem LAG() funkci pro zobrazení předchozích hodnot mezd	
 CREATE OR replace VIEW v_q4pay_2 AS
 SELECT 
 	*,
 	LAG(avg_payroll_value, 1) OVER (PARTITION BY industry_branch_name ORDER BY `year` ASC) AS previous_pay_value
 FROM v_q4pay_1
 
+-- přidal jsem výpočet meziročního nárůstu mezd	
 CREATE OR REPLACE VIEW v_q4pay_3 AS
 SELECT *,
 	round(((avg_payroll_value - previous_pay_value)/ previous_pay_value) * 100, 2) AS perc_narust_pay
 FROM v_q4pay_2
 
+-- do finální tabulky s daty mezd jsem setjně jako do finální tabulky s daty cen dal výpočet pro půměrný nárůst v procentech
 CREATE OR REPLACE TEMPORARY TABLE tt_payrise
 SELECT
 	`year`,
@@ -50,6 +59,7 @@ SELECT
 FROM v_q4pay_3
 GROUP BY `year`
 
+-- finální tabulku pro tento dotaz jsem získal spojením finálních tabulek mezd a cen
 CREATE OR REPLACE TEMPORARY TABLE tt_pay_vs_price_rise AS
 SELECT
 	tpi.`year`,
@@ -58,6 +68,7 @@ SELECT
 FROM tt_payrise tpy
 JOIN tt_pricerise tpi ON tpi.`year` = tpy.`year`
 
+-- v posledním kroce stačilo dotazovat finální tabulku s jednoduchým výpočtem rozdílu	
 SELECT 
 	*,
 	abs(avg_pay_rise - avg_price_rise) AS difference
