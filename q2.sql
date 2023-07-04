ddff6c5970895f3f63e7bd4f45b471a2df7e4cf3
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-04 13:53:37 +0200

    Added comments to q2.sql

--- q2.sql
+++ q2.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE TEMPORARY TABLE t_q2_products AS
+-- vytvořil jsem si tabulku s daty pro požadované produkty pro požadované období
+CREATE OR REPLACE TEMPORARY TABLE tt_q2_products AS
 SELECT DISTINCT 
 	`year`,
     product_name,
@@ -8,8 +9,7 @@ SELECT DISTINCT
 FROM t_jan_pelisek_project_sql_primary_final
 WHERE product_code IN (111301, 114201) AND `year` IN (2006, 2018);
 
-SELECT * FROM t_q2_products vqp 
-
+-- pohled s daty pro požadované mzdy v daném období
 CREATE OR REPLACE VIEW v_q2_payroll AS 
 SELECT DISTINCT 
 	`year`,
@@ -19,27 +19,27 @@ SELECT DISTINCT
 FROM t_jan_pelisek_project_sql_primary_final
 WHERE `year` IN (2006, 2018);
 
-SELECT * FROM v_q2_payroll
-
-CREATE OR REPLACE TEMPORARY TABLE  t_q2_avg_payroll18 AS 
+-- dočasná tabulka s průměrnou mzdou v roce 2018
+CREATE OR REPLACE TEMPORARY TABLE  tt_q2_avg_payroll2018 AS 
 SELECT
 	`year`,
 	round(avg(avg_payroll_value), 2)
 FROM v_q2_payroll vqp 
 WHERE `year` = 2018
 
-CREATE OR REPLACE TEMPORARY TABLE t_q2_avg_payroll AS
+-- dočasná tabulka s průměrnou mzdou v roce 2006
+CREATE OR REPLACE TEMPORARY TABLE tt_q2_avg_payroll AS
 SELECT
 	`year`,
 	round(avg(avg_payroll_value), 2) AS avg_payroll
 FROM v_q2_payroll vqp 
 WHERE `year` = 2006
 
-INSERT INTO t_q2_avg_payroll 
-SELECT * FROM v_q2_avg_payroll18 
+-- finální tabulku pro data mezd jsem získal vložením dat z tt_q2_avg_payroll2018 do tt_q2_avg_payroll
+INSERT INTO tt_q2_avg_payroll 
+SELECT * FROM tt_q2_avg_payroll2018 
 
-SELECT * FROM t_q2_avg_payroll
-
+-- finální data jsem získal spojením finální tabulky pro data mezd s tabulkou pro data produktů
 SELECT 
 	ps.`year`,
 	ps.product_name, 
@@ -47,5 +47,5 @@ SELECT
 	ps.price_unit,
 	pl.avg_payroll,
 	concat(round(pl.avg_payroll / ps.avg_price_value, 2), ' ', ps.price_unit) AS units_per_payroll
-FROM t_q2_products ps
-JOIN t_q2_avg_payroll pl ON ps.`year` = pl.`year` 
+FROM tt_q2_products ps
+JOIN tt_q2_avg_payroll pl ON ps.`year` = pl.`year` 
