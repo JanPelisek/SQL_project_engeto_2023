b5a076f035605fb03523fba3d31cc525ce959a0f
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-05 13:24:17 +0200

    Update q5.sql

--- q5.sql
+++ q5.sql
@@ -1,13 +1,4 @@
--- s pomocí dočasných tabulek z předchozího dotazu tt_payrise a tt_pricerise
-WITH cte_pay_price AS (
-SELECT
-	tpi.`year`,
-	tpy.`avg_pay_rise`,
-	tpi.avg_price_rise
-FROM tt_payrise tpy
-JOIN tt_pricerise tpi ON tpi.`year` = tpy.`year`
-),
-cte_GDP AS (
+WITH cte_gdp AS (
 SELECT
 	`year`,
 	country,
@@ -16,11 +7,56 @@ SELECT
 	round(((GDP - LAG(GDP) OVER (ORDER BY `year`))/ LAG(GDP) OVER (ORDER BY `year` ASC)) * 100, 2) AS gdp_rise
 FROM t_jan_pelisek_project_sql_secondary_final
 WHERE country = 'Czech Republic'
+),
+price_default AS (
+SELECT DISTINCT 
+	`year`,
+	product_name,
+	avg_price_value
+FROM t_jan_pelisek_project_sql_primary_final
+ORDER BY product_name, `year`
+),
+price_rise AS (
+SELECT
+	*,
+	LAG(avg_price_value, 1) OVER (PARTITION BY product_name ORDER BY `year` ASC) AS previous_price_value,
+	round(((avg_price_value - 	LAG(avg_price_value, 1) OVER (PARTITION BY product_name ORDER BY `year` ASC))/ 	LAG(avg_price_value, 1) OVER (PARTITION BY product_name ORDER BY `year` ASC)) * 100, 2) AS perc_narust_price
+FROM price_default
+),
+avg_price_rise AS (
+SELECT
+	`year`,
+	round(avg(perc_narust_price), 2) AS avg_price_rise
+FROM price_rise
+GROUP BY `year`
+),
+pay_default AS(
+SELECT DISTINCT
+	`year`,
+	industry_branch_name ,
+	avg_payroll_value
+FROM t_jan_pelisek_project_sql_primary_final
+ORDER BY product_name, `year`
+),
+pay_rise AS (
+SELECT
+	*, 
+	LAG(avg_payroll_value, 1) OVER (PARTITION BY industry_branch_name ORDER BY `year` ASC) AS previous_pay_value,
+	round(((avg_payroll_value - LAG(avg_payroll_value, 1) OVER (PARTITION BY industry_branch_name ORDER BY `year` ASC))/ LAG(avg_payroll_value, 1) OVER (PARTITION BY industry_branch_name ORDER BY `year` ASC)) * 100, 2) AS perc_narust_pay
+FROM pay_default
+),
+avg_pay_rise AS (
+SELECT
+	`year`,
+	round(avg(perc_narust_pay), 2) AS avg_pay_rise
+FROM pay_rise
+GROUP BY `year`
 )
-SELECT 
+SELECT
 	gdp.`year`,
-	cpp.avg_pay_rise,
-	cpp.avg_price_rise,
-	gdp.gdp_rise
-FROM cte_GDP gdp
-JOIN cte_pay_price cpp ON gdp.`year` = cpp.`year`
+	gdp.gdp_rise,
+	apy.avg_pay_rise,
+	api.avg_price_rise
+FROM cte_gdp gdp
+JOIN avg_pay_rise apy ON apy.`year` = gdp.`year`
+JOIN avg_price_rise api ON api.`year` = gdp.`year`
