36c564cdaf25ebd58a1236f6ad55bb79a4fd3648
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-22 17:27:06 +0200

    Comment update q1.sql
    Odstranění přebytečných komentářů a zjednodušení zbylých.

--- q1.sql
+++ q1.sql
@@ -1,5 +1,3 @@
--- za použití LAG() funkce vytvořil nový sloupec dat, která značila průměrnou mzdu z předchozího roku pro danné odvětví.
--- použil jsem funkci case pro nový sloupec dat, kde 1 = nynější průměrná mzda je vyšší než ta z minulého roku a 0 = nynější průměrná mzda je nižší než ta z minulého roku.
 WITH cte_q1 AS (
 SELECT 
 	`year`,
@@ -7,13 +5,13 @@ SELECT
 	avg_payroll_value,
 	LAG(avg_payroll_value,1) OVER (PARTITION BY industry_branch_name ORDER BY `year`) AS previous_payroll,
 	CASE 
-		WHEN LAG(avg_payroll_value,1) OVER (PARTITION BY industry_branch_name ORDER BY `year`) < avg_payroll_value THEN 1
-		ELSE 0
+		WHEN LAG(avg_payroll_value,1) OVER (PARTITION BY industry_branch_name ORDER BY `year`) < avg_payroll_value THEN 1 -- = nynější průměrná mzda je vyšší než ta z minulého roku
+		ELSE 0 -- = nynější průměrná mzda je nižší než ta z minulého roku
 	END is_rising
 FROM t_jan_pelisek_project_sql_primary_final
 GROUP BY industry_branch_name, `year`
 )
-SELECT * FROM cte_q1 -- nakonec jsem zadal dotaz na finální view a vytáhl si pouze data, kde mzdy vůči předchozímu roku klesly.
+SELECT * FROM cte_q1
 WHERE previous_payroll IS NOT NULL AND is_rising = 0
 ORDER BY `year`
 -- nepočítal jsem s možností, kde by byly mzdy za oba porovnávané roky stejné, protože je velice nepravděpodobná. Pokud by ta situace nastala, řešil by jsem ji tím, že by jsem do sloupce `is_rising` dal pouze hodnoty 0 a zbytek by jsem nechal NULL.
