c3a62a44ece6c763c56fbf165b6ce6b3e87aa4cf
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-22 17:29:44 +0200

    Formatting update q3.sql
    Odstranění komentářů a úprava formátování (chyby v odazení)

--- q3.sql
+++ q3.sql
@@ -1,24 +1,24 @@
--- pro řešení tohoto dotazu jsem využil tří common table expressions, postupně jsem pomocí nich získával potřebné údaje
 WITH cte_q3 AS (  
-SELECT DISTINCT -- select pro výchozí data
+SELECT DISTINCT 
 	`year`,
 	product_name,
 	avg_price_value
 FROM t_jan_pelisek_project_sql_primary_final
 ORDER BY product_name, `year`
-    ),
+),
 cte2_q3 AS (
-    SELECT -- select s funkcí LAG() pro výpočet předchozí ceny produktu
+SELECT 
 	*,
 	LAG(avg_price_value, 1) OVER (PARTITION BY product_name ORDER BY `year` ASC) AS previous_price_value
 FROM cte_q3
-	),
+),
 cte3_q3 AS (
-SELECT *, -- select s výpočtem meziročního procentuálního nárůstu cen produktů
+SELECT 
+	*,
 	round(((avg_price_value - previous_price_value)/ previous_price_value) * 100, 2) AS perc_narust
 FROM cte2_q3
 )
-SELECT -- finální select s výpočtem pro průměrný percentuální nárůst produktů za celé sledované období
+SELECT
 	product_name,
 	round(avg(perc_narust), 2) AS perc_narust_final
 FROM cte3_q3
