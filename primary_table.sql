28d928a27b632ae5d4bf691cf41f457a14ac6d9b
Jan Pelíšek <52496899+JanPelisek@users.noreply.github.com>
2023-07-22 17:39:06 +0200

    Formatting update primary_table.sql
    Odstranění přebytečných komentářů a zbytečných dotazů.

--- primary_table.sql
+++ primary_table.sql
@@ -1,23 +1,17 @@
--- tvorba primární tabulky t_jan_pelisek_project_SQL_primary_final
--- k vytvoření této tabulky bylo zapotřebí spojit tabulky czechia_price, czechia_price_category, czechia_payroll a czechia_payroll_industry_brunch
--- začal jsem vytvořením dvou dočasných tabulek. První jsem vytvořil spojením tabulek s daty o cenách základních potravin a druhou spojením tabulek s daty mezd. Při tomto kroku jsem si z tabulek vytáhl pouze data, která nás v tomto projektu budou zajímat (tzn. v první tabulce jsem si data o časovém období upravil na roky a ceny potravin jsem si pro dané roky zprůměroval. V  druhé tabulce jsem si vybral pouze data o mzdách pro přepočtený počet zaměstnanců na plný úvazek a pouze pro období shodné s první tabulkou). V druhé tabulce jsem se pomocí LEFT JOIN zbavil NULL hodnot a upravil období tak, aby se shodovalo s obdobím v první tabulce.
+-- začal jsem vytvořením dvou dočasných tabulek. První jsem vytvořil spojením tabulek s daty o cenách základních potravin a druhou spojením tabulek s daty mezd. 
 -- po vytvoření těchto dočasných tabulek, je stačilo pouze spojit a vytvořit tím finální tabulku, ve které nebylo potřeba nic upravovat.
--- První jsem vytvořil spojením tabulek s daty o cenách základních potravin a druhou spojením tabulek s daty mezd. Při tomto kroku jsem si z tabulek vytáhl pouze data, která nás v tomto projektu budou zajímat
-
 
 CREATE TEMPORARY TABLE czechia_price_temporary AS
 SELECT
-	YEAR(cp.date_from) AS 'year', -- data o časovém období jsem upravil na roky
+	YEAR(cp.date_from) AS 'year', 
 	cpc.code,
 	cpc.name,
-	round(AVG(cp.value),2) AS 'avg_price_value', -- ceny potravin jsem si pro dané roky zprůměroval
+	round(AVG(cp.value),2) AS 'avg_price_value', 
 	cpc.price_unit 
 FROM czechia_price cp
 JOIN czechia_price_category cpc
 	ON cp.category_code = cpc.code
 GROUP BY YEAR(cp.date_from), name 
-
-SELECT * FROM czechia_price_temporary
 
 CREATE TEMPORARY TABLE czechia_payroll_temporary AS
 SELECT
@@ -28,16 +22,13 @@ SELECT
 FROM czechia_payroll cp
 LEFT JOIN czechia_payroll_industry_branch cpib -- pomocí LEFT JOIN jsem se zbavil NULL hodnot
 	ON cp.industry_branch_code = cpib.code
-WHERE cp.value_type_code = 5958 -- pouze data o mzdách
-	AND cp.calculation_code = 200 -- pouze data pro přepočtený počet zaměstnanců na plný úvazek
-	AND cp.payroll_year BETWEEN 2006 AND 2018 -- pouze pro období shodné s první tabulkou
+WHERE cp.value_type_code = 5958 -- = pouze data o mzdách
+	AND cp.calculation_code = 200 -- = pouze data pro přepočtený počet zaměstnanců na plný úvazek
+	AND cp.payroll_year BETWEEN 2006 AND 2018
 	AND cp.industry_branch_code IS NOT NULL
 GROUP BY cpib.name, cp.payroll_year
 ORDER BY cpib.name, cp.payroll_year
 
-SELECT * FROM czechia_payroll_temporary
-
--- po vytvoření těchto dočasných tabulek, je stačilo pouze spojit a vytvořit tím finální tabulku, ve které nebylo potřeba nic upravovat.
 CREATE OR REPLACE TABLE t_jan_pelisek_project_SQL_primary_final AS
 SELECT
 	cpayt.year,
